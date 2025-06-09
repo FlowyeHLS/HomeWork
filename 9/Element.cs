2952de9bb7c9ba0624e03351a3252547f221ca16
FlowyeHLS <king2015@list.ru>
2025-06-09 18:58:00 +0500

    Create Element.cs

---
+++ 9/Element.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ForwardList
+{
+	internal class Element
+	{
+
+		public int Data { get; set; }
+		public Element pNext { get; set; }
+		public Element pRev { get; set; }
+
+		//public Element(int Data, Element pNext = null, Element Next = null) 
+		//{
+		//    this.Data = Data;
+		//	this.pNext = pNext;
+		//	this.Next = Next;
+		//	Console.WriteLine($"EConstructor:\t{GetHashCode()}");
+		//}
+
+
+		public Element(int Data, Element pNext = null, Element pRev = null)
+		{
+			this.Data = Data;
+			this.pNext = pNext;
+			this.pRev = pRev;
+
+			Console.WriteLine($"EConstructor:\t{GetHashCode()}");
+		}
+
+		~Element()
+		{
+			Console.WriteLine($"EDestructor:\t{GetHashCode()}");
+		}
+	}
+}
